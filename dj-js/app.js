96abb48b8011232c4275f12e4250cd794fda8079
Cole DeVore <c16devore@gmail.com>
2022-05-16 18:56:49 -0700

    completed dom debig assignment

---
+++ dj-js/app.js
@@ -0,0 +1,13 @@
+var square = document.getElementById("square1")
+
+// square.addEventListener("click", myFunction);
+// function myFunction(){
+//     console.log("click")
+// }
+// event listener example^^
+
+
+square.addEventListener("mouseover",myFunction1);
+function myFunction1(){
+    square.style.color = "blue"
+}
